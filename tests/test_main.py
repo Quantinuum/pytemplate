52660653d3130c1ded4926ff9d94ef12cb4cd571
Kartik Singhal <kartik.singhal@quantinuum.com>
2026-03-19 09:29:37 -0500

    fix: use public pytest capture fixture typing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 from pytemplate.main import hello_world, main
 
 if TYPE_CHECKING:
-    from _pytest.capture import CaptureFixture
+    import pytest
 
 
 def test_hello_world() -> None:
@@ -13,7 +13,7 @@ def test_hello_world() -> None:
     assert hello_world() == "Hello, World!"
 
 
-def test_main(capsys: CaptureFixture[str]) -> None:
+def test_main(capsys: pytest.CaptureFixture[str]) -> None:
     """Write the greeting to stdout."""
     main()
     captured = capsys.readouterr()
